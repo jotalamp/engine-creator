37a193f06fa045061eaa9a0817c8692f6d526789
Jotalamp <joonaslampinen1@gmail.com>
2025-05-20 14:38:12 +0300

    Readme updated

--- README.md
+++ README.md
@@ -1,8 +1,12 @@
-# engine-creator
+# Engine Creator v0.1
 
-### Can change values of template_engine.mr and save to file
+## Tool for creating engines for AngeTheGreat's [Engine Simulator](https://github.com/ange-yaghi/engine-sim)
+
+### Can change values and units of template_engine.mr and save to file
 
 ```
+gh repo clone jotalamp/engine-creator -- --recurse-submodules 
+mkdir build
 cd build
 cmake .. && make && make install && ../bin/test && ../bin/engine-creator
 ```
--- src/engine-creator/EngineCreator.h
+++ src/engine-creator/EngineCreator.h
@@ -60,7 +60,7 @@ public:
     bool setCreatedEngineFileName(const std::string &newFileName);
 
     void addEditableValue(unsigned int lineNumber, const std::string &name, const std::string &editableValue);
-    void addEditableValue(unsigned int lineNumber, const std::string &name, double editableValue, unsigned int decimals=3);
+    void addEditableValue(unsigned int lineNumber, const std::string &name, double editableValue, unsigned int decimals = 3);
     void addEditableValue(unsigned int lineNumber, const std::string &name, int editableValue);
 
     unsigned int getLineCount() const;
@@ -80,7 +80,7 @@ private:
     std::unordered_map<std::string, EditableIntegerValue> editableIntegerValuesByName;
 
     std::vector<std::string> editedLines;
-    static const inline std::string path = "../bin/";
+    static const inline std::string path = "./bin/";
     static const inline std::string templateEngineName = "template_engine.mr";
     static inline std::string createdEngineName = "created_engine.mr";
     unsigned int linesCount = 0;
